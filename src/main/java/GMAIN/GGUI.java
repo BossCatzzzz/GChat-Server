372f34cd8fe81f3fd1e7780f99df000a3deb576b
Quốc Tạo Lý <Tao_ly@professionis.onmicrosoft.com>
2022-08-27 11:08:11 +0700

    GChat packets Project by Gic

--- src/main/java/GMAIN/GGUI.java
+++ src/main/java/GMAIN/GGUI.java
@@ -1,7 +1,6 @@
 package GMAIN;
 
 import com.formdev.flatlaf.FlatDarkLaf;
-import com.mycenter.gobject.GPacket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
